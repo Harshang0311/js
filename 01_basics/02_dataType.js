d1af24700e6a47ad039af6f861a6244e352f4e8b
Harshang0311 <24cp432@bvmengineering.ac.in>
2025-09-15 20:37:45 +0000

    data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,17 @@
+"use stricts"; //treat all JS code as newer version
+console.log(3+3)
+let name
+
+/*
+number=> 2 to power 53
+bigint
+string=> ""
+boolean=true/false
+null=>standalone value
+undefined=>
+symbol =>unique
+
+*/
+console.log(typeof null); //output:- object
+console.log(typeof undefined); //output:-undefined
+
